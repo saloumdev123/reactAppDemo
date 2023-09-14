97bdd69b5b74e114f16a8f6d2e5fa5450ba7b925
papa saloum sene <saloumdev@gmail.com>
2023-09-14 19:57:38 +0000

    admin dashboard first version

---
+++ roadmap/AdminPanel/components/Sidebar.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { BsCart3, BsFillArchiveFill, BsFillGearFill, BsFillGrid3X3GapFill, BsGrid1X2Fill, BsListCheck, BsMenuButtonWideFill, BsPeopleFill} from 'react-icons/bs'
+
+const Sidebar = () => {
+  return ( 
+   <aside id='sidebar'>
+      <div className='sidebar-title'>
+          <div className='sidebar-brand'>
+              <BsCart3 className='icon-header'/> SHOP
+          </div>
+          <span className='ico close_icon'>X</span>
+      </div>
+
+      <ul className='sidebar-list'>
+        <li className='sidebar-list-item'>
+            <a href="">
+                <BsGrid1X2Fill className='icon'/> Dashboard
+            </a>
+        </li>
+        <li className='sidebar-list-item'>
+            <a href="">
+                <BsFillGrid3X3GapFill className='icon'/> Categories
+            </a>
+        </li>
+        <li className='sidebar-list-item'>
+            <a href="">
+                <BsFillArchiveFill className='icon'/> Products
+            </a>
+        </li>
+        <li className='sidebar-list-item'>
+            <a href="">
+                <BsPeopleFill className='icon'/> Customer
+            </a>
+        </li>
+        <li className='sidebar-list-item'>
+            <a href="">
+                <BsListCheck className='icon'/> Inventory
+            </a>
+        </li>
+        <li className='sidebar-list-item'>
+            <a href="">
+                <BsMenuButtonWideFill className='icon'/> Reports
+            </a>
+        </li>
+        <li className='sidebar-list-item'>
+            <a href="">
+                <BsFillGearFill className='icon'/> Setting
+            </a>
+        </li>
+      </ul>
+   </aside>
+  )
+}
+
+export default Sidebar
---
+++ roadmap/AdminPanel/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Home from '../components/Home'
+import Sidebar from '../components/Sidebar'
+import Header from '../components/Header'
+
+function App() {
+  
+  return (
+    <div className='grid-container'>
+     <Header />
+     <Home />
+     <Sidebar />
+    </div>
+  )
+}
+
+export default App
